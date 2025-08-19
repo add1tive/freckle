3a9d6c8e10a2fb71eca2d9cbc06e76f73d9c744b
add1tive <225592744+add1tive@users.noreply.github.com>
2025-08-19 13:38:58 +0200

    [Web] Prepare for Prettier

--- web/src/routes/docs/expressions-table/+page.svelte
+++ web/src/routes/docs/expressions-table/+page.svelte
@@ -1,27 +1,27 @@
-<!--
-    Freckle - a Discord app and its website
-    Copyright (C) 2025 add1tive
-
-    This file is part of Freckle.
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
--->
-
 <!-- Since this isn't a .svx file, I either have to manually add header links + toc or set up jank -->
 <!-- I chose the former -->
 
 <script lang="ts">
+    /*
+     * Freckle - a Discord app and its website
+     * Copyright (C) 2025 add1tive
+     *
+     * This file is part of Freckle.
+     *
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with this program. If not, see <http://www.gnu.org/licenses/>.
+     */
+
     export const title = "Textbox character expressions table";
 
     import { base } from "$app/paths";
